fce5b917d2b5df8f5abd4f76dd6a7b572166e0c2
HW, Siew <hw.siew@actiweb.co>
2021-03-11 11:42:47 +0800

    Changed: refactored main function for testing

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ from ace_pointer.myapp import PokemonSearchEngine
 def main():
 	"""The entry point of the program"""
 
-	# banner to display at the beginning of program 
-	banner = """
-~ Welcome to Ace pointer Pokemon Search Enginee ~
-To query a pokemon, please type an id or name follows by enter
-To quit the program, please type 0 
- 	"""
-
-	print(banner)
-
 	# initialize pokemon search engine
 	engine = PokemonSearchEngine()
 
@@ -25,13 +16,23 @@ To quit the program, please type 0
 	while id_or_name != '0':
 		
 		if id_or_name != '':
-			x = engine.query(id_or_name)
+			engine.query(id_or_name)
 		else:
 			print('You had entered nothing')
 
-		print() # empty line to separate query
+		#print() # empty line to separate query
 
 		id_or_name = input('Search more or enter 0 to quit: ')
 
 if __name__ == '__main__':
-    main()
+
+	# banner to display at the beginning of program 
+	banner = """
+~ Welcome to Ace pointer Pokemon Search Enginee ~
+To query a pokemon, please type an id or name follows by enter
+To quit the program, please type 0 
+ 	"""
+	 
+	print(banner)
+
+	main()
